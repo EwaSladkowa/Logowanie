36462cae001b54284f8bc869732e5c9564a53764
EwaSladkowa <sladkowayewa126@gmail.com>
2025-01-17 14:48:36 +0100

    Add files via upload

---
+++ qwertyuiop.js
@@ -0,0 +1,6 @@
+
+function Login(){
+    var Login = document.getElementById('login-input').value;
+    var Password = document.getElementById('password-input').value;
+    alert("Login: " + Login + "  Password: " + Password);
+}
