ca31487692216948609b3cad4ad621a849fff06b
EwaSladkowa <sladkowayewa126@gmail.com>
2025-04-11 16:47:52 +0200

    .

--- Login.js
+++ Login.js
@@ -4,9 +4,3 @@ function Login(){
     var Password = document.getElementById('password-input').value;
     alert("Login: " + Login + "  Password: " + Password);
 }
-
-function Zarejestrowac(){
-    var rejestracja = document.getElementById('login-input').value;
-    var Password = document.getElementById('password-input').value;
-    alert("Login: " + Login + "  Password: " + Password);
-}
--- Login.php
+++ Login.php
@@ -1,30 +1,48 @@
 <?php
+if (!empty($_POST["Login"]) && !empty($_POST["Haslo"]) && !empty($_POST["button"])){
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "logowanie";
 
-$connection = mysqli_connect(hostname: $servername, username: $username, password: $password = "",database: $dbname);
+$link = mysqli_connect(hostname: $servername, username: $username, password: $password = "",database: $dbname);
 
 
-if (!$connection ){
-    die(mysqli_error(mysql: $connection));
+if (!$link ){
+    die(mysqli_error(mysql: $link));
 }
 
 $Login = $_POST["Login"];
 $Haslo = $_POST["Haslo"];
+$action = $_POST["button"];
 
-mysqli_query(
-    $connection,
-    query: INSERT INTO `uzytkownicy`(`Login`, `hASŁO`) VALUES ('$Login','$Haslo');
-);
-mysqli_close(mysql: $connection);
+if (!empty($_POST["Haslo"]) and !empty($_POST["Login"])){
 
+if  ($action == "Login"){
+    echo "ujg";
+    $query = "SELECT `Login`, `Haslo` FROM `uzytkownicy` WHERE login='$Login' AND password='$Haslo'";
+    $res =  mysqli_query($query, $link);
+    $user = mysqli_fetch_assoc($res);
+    
+    if ($user) {
+        echo "aaaaaaaaaaaaaaaaaaaa";
+    } else {
+        echo ":(";
+    }
 
+}
+elseif ($action == "Registracja") {
+       $query = "INSERT INTO `uzytkownicy`(`Login`, `Haslo`) VALUES ('$Login','$Haslo')" ;
+       $res =  mysqli_query($query, $link);
+    
+       if ($res){
+        echo "Dodano do bazy";
+       }else{
+        echo ":((";
+       }
+}
 
-
-
-
-
-
+}
+mysqli_close(mysql: $link);
+}
 ?>
